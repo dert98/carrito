2b6f041c5e70b30be3804d75e1b0e762fa083419
Vanesa Astrada <vane.200980pancho@gmail.com>
2024-02-26 19:35:14 -0300

    img folder restructuring
    img folder restructuring with the creation of subfolders when creating a product

--- carrito.php
+++ carrito.php
@@ -11,7 +11,7 @@
 <div>
     <h2>Products</h2>
     <ul>
-        <li v-for="product in products" :key="product.id">
+        <li v-for="product in cart" :key="product.id">
             {{ product.name }} - ${{ product.price }}
             <button @click="addToCart(product)">Add to Cart</button>
         </li>
@@ -21,21 +21,8 @@
         new Vue({
             el: '#app',
             data: {
-                products: []
+                cart: []
             },
-            mounted() {
-                // Realizar una solicitud HTTP para obtener los productos desde la API PHP
-                axios.get('obtener_productos.php')
-                    .then(response => {
-                        this.products = response.data;
-                    })
-                    .catch(error => {
-                        console.error('Error al obtener los productos:', error);
-                    });
-            },
-            methods: {
-                // Agregar métodos para manejar el carrito de compras
-            }
         });
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
