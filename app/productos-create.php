327ad93119abef2f57c7b29b7633070aca53daca
Oscar <43421688+dert98@users.noreply.github.com>
2024-03-08 21:51:13 -0300

    Update productos-create.php
    select multiple images

--- app/productos-create.php
+++ app/productos-create.php
@@ -54,11 +54,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Create Record</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
+
 <body>
     <section class="pt-5">
         <div class="container-fluid">
@@ -69,47 +72,53 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </div>
                     <p>Please fill this form and submit to add a record to the database.</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>nombre</label>
+                            <input type="text" name="nombre" maxlength="100" class="form-control"
+                                value="<?php echo $nombre; ?>">
+                            <span class="form-text">
+                                <?php echo $nombre_err; ?>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label>descripcion</label>
+                            <textarea name="descripcion" class="form-control"><?php echo $descripcion ; ?></textarea>
+                            <span class="form-text">
+                                <?php echo $descripcion_err; ?>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label>precio</label>
+                            <input type="text" name="precio" class="form-control" value="<?php echo $precio; ?>">
+                            <span class="form-text">
+                                <?php echo $precio_err; ?>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label>categoria</label>
+                            <select class="form-control" id="categoria_id" name="categoria_id">
+                                <?php
+                                    $sql = "SELECT id, nombre FROM categorias"; // Modificado para seleccionar solo id y nombre
+                                    $result = mysqli_query($link, $sql);
+                                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                                        $categoria_id = $row['id']; // ID de la categoría
+                                        $nombre_categoria = $row['nombre']; // Nombre de la categoría
+                                        if ($categoria_id == $categoria_id_selected) {
+                                            echo '<option value="' . $categoria_id . '" selected>' . $nombre_categoria . '</option>';
+                                        } else {
+                                            echo '<option value="' . $categoria_id . '">' . $nombre_categoria . '</option>';
+                                        }
+                                    }
+                                ?>
+                            </select>
+                            <span class="form-text"><?php echo $categoria_id_err; ?></span>
+                        </div>
 
                         <div class="form-group">
-                                <label>nombre</label>
-                                <input type="text" name="nombre" maxlength="100"class="form-control" value="<?php echo $nombre; ?>">
-                                <span class="form-text"><?php echo $nombre_err; ?></span>
-                            </div>
-						<div class="form-group">
-                                <label>descripcion</label>
-                                <textarea name="descripcion" class="form-control"><?php echo $descripcion ; ?></textarea>
-                                <span class="form-text"><?php echo $descripcion_err; ?></span>
-                            </div>
-						<div class="form-group">
-                                <label>precio</label>
-                                <input type="text" name="precio" class="form-control" value="<?php echo $precio; ?>">
-                                <span class="form-text"><?php echo $precio_err; ?></span>
-                            </div>
-						<div class="form-group">
-                                <label>categoria_id</label>
-                                    <select class="form-control" id="categoria_id" name="categoria_id">
-                                    <?php
-                                        $sql = "SELECT *,id FROM categorias";
-                                        $result = mysqli_query($link, $sql);
-                                        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-                                            array_pop($row);
-                                            $value = implode(" | ", $row);
-                                            if ($row["id"] == $categoria_id){
-                                            echo '<option value="' . "$row[id]" . '"selected="selected">' . "$value" . '</option>';
-                                            } else {
-                                                echo '<option value="' . "$row[id]" . '">' . "$value" . '</option>';
-                                        }
-                                        }
-                                    ?>
-                                    </select>
-                                <span class="form-text"><?php echo $categoria_id_err; ?></span>
-                            </div>
-						<div class="form-group">
-                                <label>imagen</label>
-                                <input type="text" name="imagen" maxlength="255"class="form-control" value="<?php echo $imagen; ?>">
-                                <span class="form-text"><?php echo $imagen_err; ?></span>
-                            </div>
-
+                            <label>imagen</label>
+                            <input name="imagenes[]" type="file" class="form-control" multiple>
+                            <span class="form-text"><?php echo $imagen_err; ?></span>
+                        </div>
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="productos-index.php" class="btn btn-secondary">Cancel</a>
                     </form>
@@ -117,8 +126,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
         </div>
     </section>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
