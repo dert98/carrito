d1cf152aff68195a108fb41679e3e84309128b7b
Oscar <dert98@hotmail.com>
2024-03-11 20:11:59 -0300

    images detalle

--- productos-detalle.php
+++ productos-detalle.php
@@ -18,6 +18,7 @@ if(isset($_GET["id"]) && !empty($_GET["id"])){
         
         // Verificar si se encontró el producto
         if($row) {
+            // Mostrar el HTML con los detalles del producto y el slider
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -61,8 +62,31 @@ if(isset($_GET["id"]) && !empty($_GET["id"])){
             </div>
         </div>
     </section>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" style="width: 200px; height: 200px;">
+            <div class="carousel-item active">
+                <img src="img/p1/1.webp" class="d-block w-100" alt="Slide 1">
+            </div>
+            <div class="carousel-item">
+                <img src="img/p1/2.webp" class="d-block w-100" alt="Slide 2">
+            </div>
+            <div class="carousel-item">
+                <img src="img/p1/3.webp" class="d-block w-100" alt="Slide 3">
+            </div>
+            <!-- Añade más imágenes según sea necesario -->
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.12.1/dist/umd/popper.min.js" integrity="sha384-FZNtAtjJ8Sb4W+3G2GtFsJ/xXIVbpJ5rCVa+a7CLQ2Se+U3N2QNOZlsOw+2jnYlS" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
