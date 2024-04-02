d32b7bf5f58658a9520ce393b37df2c0c99ce6c5
Mariano <49082449+marianocastro2801@users.noreply.github.com>
2024-04-02 15:25:57 -0300

    add files
    base de datos actualizada! con productos para cada categoria 
    para reflejar los cambios importar nueva db y actualizar la carpeta img

--- web.sql
+++ web.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-03-2024 a las 20:18:48
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4 
+-- Tiempo de generación: 02-04-2024 a las 20:22:38
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,11 +37,11 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`) VALUES
-(1, 'Cargadores'),
-(2, 'Parlantes'),
-(3, 'Computadoras'),
-(4, 'Notebook'),
-(5, 'Monitores');
+(1, 'Relojes inteligentes'),
+(2, 'Parlantes Bluetooth'),
+(3, 'Auricular Inalámbrico'),
+(4, 'Auriculares (Estilos populares)'),
+(5, 'Fundas Celulares');
 
 -- --------------------------------------------------------
 
@@ -63,14 +63,19 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`, `categoria_id`, `imagen`) VALUES
-(4, 'pc i5 12generacion', 'pc', 9999.00, 1, 'p_1/1.webp'),
-(5, 'tcl c34500', 'jkh lkjhlh lk', 9999.00, 4, 'p_1/4.webp'),
-(6, 'pc i5 12generacion', 'pc', 9999.00, 3, 'p_2/2.webp'),
-(7, 'Laptop ASUS VivoBook', 'Laptop ASUS VivoBook con procesador Intel Core i5, 8GB de RAM y 512GB de SSD', 799.99, 1, 'p_1/1.webp'),
-(8, 'Monitor HP 27 pulgadas', 'Monitor HP de 27 pulgadas con resolución Full HD y tecnología IPS', 249.99, 5, 'p_1/5.webp'),
-(9, 'Teclado mecánico Corsair K70', 'Teclado mecánico Corsair K70 con retroiluminación RGB y switches Cherry MX Brown', 149.99, 3, 'p_2/2.webp'),
-(10, 'Mouse inalámbrico Logitech MX Master 3', 'Mouse inalámbrico Logitech MX Master 3 con sensor de alta precisión y botón de desplazamiento rápido', 99.99, 3, 'p_1/4.webp'),
-(11, 'Disco duro externo Samsung Portable SSD T5', 'Disco duro externo Samsung Portable SSD T5 con capacidad de 1TB y conexión USB-C', 149.99, 4, 'p_1/4.webp');
+(1, 'Reloj inteligente S300ultar', 'S300ultar\r\nel reloj\r\nCorrea\r\ncaja de empaque\r\nmanuales\r\ncable de energía', 120.00, 1, 'p_1/1.webp'),
+(2, 'Auricular Inalámbrico M28', 'Chip: Jerry 6983\r\nVersión de bluetooth: V5.1\r\nCapacidad de la batería de los auriculares：50mAh\r\nCapacidad de la batería de la caja de almacenamiento: 2000mAh\r\nDistancia de transmisión:>10m \r\nTiempo de llamada: 4H                                                                Tiempo de reproducción: 4-5H                                                     Tiempo de carga: 1-2h\r\nModo de funcionamiento: Táctil\r\nAccesorios: Cable, Tapa del oído, Instrucciones', 110.00, 3, 'p_1/4.webp'),
+(3, 'Reloj inteligente S9 max', 'reloj inteligente\r\nS9 max\r\n\r\nel reloj\r\nCorrea\r\ncaja de empaque\r\nmanuales\r\ncable de energía\r\nCarga inalámbrica, batería de buena calidad, correa de reloj única', 80.00, 1, 'p_2/2.webp'),
+(7, 'Reloj inteligente S9PRO', 'Reloj inteligente S9PRO nuevo set dos en uno con Watch9 + auriculares de 5ª generación + carga inalámbrica, pantalla grande de alta definición 2.3, correas con doble hebilla', 90.00, 1, 'p_1/1.webp'),
+(8, 'Funda Xiaomi Redmi Note11Pro 4G', 'Redmi Note11Pro 4G\r\nFundas 1.8mm TPU imitacion de silicona liquida', 25.00, 1, 'p_1/5.webp'),
+(9, 'Parlantes Bluetooth', 'colores negro/rojo/azul/verde\r\nSoporte bluetooth, TF  card , USB, FM, se puede conectar\r\nmaterial del producto: Plástico ABS\r\nespecificaciones bluetooth: Bluetooth 5.0 Jerry\r\nDistancia de transmisión: 10M\r\nEspecificaciones de los altavoces: 52MM*2   4ohmios3vatios*2\r\nespecificación de potencia: 3W*2\r\nespecificación de la batería: 1100mAh 18650\r\ntiempo de funcionamiento: 2-3H', 80.00, 2, 'p_2/2.webp'),
+(10, 'Auricular Inalámbrico M32', 'Chip: Jerry 6963\r\nVersión de bluetooth: V5.1\r\nBocina: 8MM\r\nCapacidad de la batería de la caja de almacenamiento: 800MAH\r\nDistancia de transmisión:>10m \r\nTiempo de llamada: 4H                                                                 Tiempo de reproducción: Unos 6H\r\nTiempo de carga: 1-2h\r\nModo de funcionamiento: Táctil\r\nAccesorios: Cable, Tapa del oído, Instrucciones\r\nFunción: interruptor de modo dual, interruptor hacia arriba y hacia abajo, adición de volumen y sustracción, llamada/rechazo/reproducción/pausa. Asistente de voz de convocatoria', 100.00, 3, 'p_1/4.webp'),
+(11, 'Parlante (bluetooth)', 'negro/caqui/rosa/azul\r\nAdmite Bluetooth,  tarjeta TF,  AUX USB,  micrófono\r\nMaterial del producto:  ABS,  malla de hierro.\r\nEspecificación de dientes azules:  Bluetooth 5.3 Jerry\r\nDistancia de transmisión:  10M\r\nEspecificación del altavoz:  57 mm 4Ω5W\r\nEspecificación del amplificador de potencia:  5W * 1\r\nEspecificación de la batería:  1200 mAh\r\nTiempo de funcionamiento:  2-3H', 90.00, 2, 'p_1/4.webp'),
+(12, 'Funda Samsung A03 164 (universal)', 'Samsung A03 164 (universal)\r\nFundas 1.8mm TPU imitacion de silicona liquida', 25.00, 1, ''),
+(13, 'Funda iPhone 12 Pro', 'iPhone 12 Pro\r\nFundas 1.8mm TPU imitacion de silicona liquida', 25.00, 1, '/img/p1/WhatsApp Image 2024-02-06 at 12.12.21 PM.jpeg'),
+(14, 'Reloj Inteligente Nuevo Z87Ultra', 'Reloj inteligente\r\nNuevo Z87Ultra\r\nUltra 2 agrega un nuevo dial de zoom infinito, gestos para contestar llamadas y apagar el despertador. La última esfera de escaneo de radar, se enviaron tres pares de correas de reloj (correa de acero ➕ silicona trenzada ➕ correa marina)asas', 120.00, 1, ''),
+(16, 'Auricular Popular JR01', 'Chip: Jerry\r\nVersión Bluetooth: 5.3\r\nCuerno: φ8mm\r\nBatería de auriculares: 30mah\r\nBatería de almacenamiento: 300mah\r\nTiempo de espera de apagado: alrededor de 180 días\r\nTiempo de música: 5 horas\r\nTiempo de conversación: 5 horas\r\nTiempo de carga de los auriculares: unos 40 minutos\r\nTiempo de carga de la caja de carga: aproximadamente 1 hora\r\nDistancia de comunicación: 10M\r\nInterfaz de carga: Tipo-C\r\nVoltaje de carga: DC5V/1A', 90.00, 4, NULL),
+(17, 'Auricular popular JR03', 'Microprocesador: Jerry 6973D4\r\nVersión Bluetooth: 5.3\r\nCuerno: anillo de cobre real de φ13 mm\r\nBatería de auriculares: 25mah\r\nBatería de almacenamiento: 200mah\r\nTiempo de espera de apagado: alrededor de 180 días\r\nTiempo de música: 3-4 horas\r\nTiempo de conversación: 3-4 horas\r\nTiempo de carga de los auriculares: alrededor de 1 minuto\r\nTiempo de carga del contenedor de carga: alrededor de 1,5 horas\r\nDistancia de comunicación: 10M\r\nInterfaz de carga: Tipo-C\r\nVoltaje de carga: DC5V/1A\r\nColor: negro, blanco', 110.00, 4, NULL);
 
 -- --------------------------------------------------------
 
@@ -128,7 +133,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
